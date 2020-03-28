57088dbc1799a9db5cbb5b8c9b5ee42744b20f44
Ronald <rnor3536@uni.sydney.edu.au>
2020-03-28 13:50:21 +1100

    Complete setup

--- moviepresenter/src/main/java/com/moviepresenter/moviepresenter/HelloController.java
+++ moviepresenter/src/main/java/com/moviepresenter/moviepresenter/HelloController.java
@@ -1,30 +1,29 @@
 package com.moviepresenter.moviepresenter;
 
-
+import com.moviepresenter.moviepresenter.model.PingResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Date;
-import java.util.logging.Logger;
-
-import com.moviepresenter.moviepresenter.model.PingRequest;
-import com.moviepresenter.moviepresenter.model.PingRequest;
-import com.moviepresenter.moviepresenter.model.CoreServiceGrpc;
+import java.util.Collection;
 
 
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() throws IOException {
+    @RequestMapping(path="/", produces = {"application/json"})
+    @ResponseBody
+    public ResponseEntity index() {
+
+        Map<String, Object> map = new HashMap<String, Object>();
 
         Date date = new Date();
 
@@ -35,28 +34,13 @@ public class HelloController {
                 .build();
 
         HelloClient client = new HelloClient(channel);
-        String message = client.ping(date.getTime());
+        Collection<PingResponse> responses = client.ping(date.getTime());
 
-        return "Greetings from Spring Boot! I am the movie presenter! "+ message;
+        responses.forEach(item-> {
+                    map.put(item.getCountry(), item.getTotalConfirmed());
+                });
+
+        return ResponseEntity.ok(map);
     }
-
-//    public static String getText(String url) throws IOException {
-//        URL website = new URL(url);
-//        URLConnection connection = website.openConnection();
-//        BufferedReader in = new BufferedReader(
-//                new InputStreamReader(
-//                        connection.getInputStream()));
-//
-//        StringBuilder response = new StringBuilder();
-//        String inputLine;
-//
-//        while ((inputLine = in.readLine()) != null) {
-//            response.append(inputLine);
-//        }
-//
-//        in.close();
-//
-//        return response.toString();
-//    }
 
 }
